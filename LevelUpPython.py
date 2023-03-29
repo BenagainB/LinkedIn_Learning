47760d948f7f96a5631f94a01a4e8bae8a93a5a5
BenagainB <91215159+BenagainB@users.noreply.github.com>
2023-03-29 08:43:02 -0700

    add save and load dict functions

--- LevelUpPython.py
+++ LevelUpPython.py
@@ -1,65 +1,103 @@
-#Lessons from the Level Up: Python LinkedIn Learning Series
-#https://www.linkedin.com/learning/level-up-python
+# Lessons from the Level Up: Python LinkedIn Learning Series
+# https://www.linkedin.com/learning/level-up-python
 import re
 import time
 import random
+import pickle
 
-#find prime factors
 def get_prime_factors(number):
-  factors = []
-  divisor = 2
-  while divisor <= number:
-    if number % divisor == 0:
-      factors.append(divisor)
-      number = number // divisor
-    else:
-      divisor += 1
-  return factors
+    """find prime factors"""
+    factors = []
+    divisor = 2
+    while divisor <= number:
+        if number % divisor == 0:
+            factors.append(divisor)
+            number = number // divisor
+        else:
+            divisor += 1
+    return factors
 
-#identify a plindrome
+
 def is_palindrome(phrase):
-  #'Go hang a salami - I'm a lasanga hog' will evaluate True
-  forwards = ''.join(re.findall(r'[a-z]+', phrase.lower()))
-  backwards = forwards[::-1]
-  return forwards == backwards
+    """identify a palindrome"""
+    #'Go hang a salami - I'm a lasagna hog.' will evaluate True
+    forwards = ''.join(re.findall(r'[a-z]+', phrase.lower()))
+    backwards = forwards[::-1]
+    return forwards == backwards
 
-#sort a string
+
 def sort_words(input):
-  #sorts 'apple ORANGE banana' as 'apple banana ORANGE' ignoring case for sort but preserving for output
-  words = input.split()
-  words = [w.lower() + w for w in words]
-  words.sort()
-  words = [w[len//2:] for w in words]
-  return ' '.join(words)
+    """sort a string"""
+    #sorts 'apple ORANGE banana' as 'apple banana ORANGE' ignoring case for sort but preserving for output
+    words = input.split()
+    words = [w.lower() + w for w in words]
+    words.sort()
+    words = [w[len(w)//2:] for w in words]
+    return ' '.join(words)
 
-#find all list items
+
 def index_all(search_list, item):
-  #searching 'example = [[[1,2,3], 2, [1,3]], [1,2,3]]' for presence of 'example, 2' resutls in '[[0,0,1], [0,1], [1,1]]'
-  #searching 'example = [[[1,2,3], 2, [1,3]], [1,2,3]]' for presence of 'example, [1,2,3]' resutls in '[[0,0], [1]]'
-  for index, value in enumerate(search_list):
-    if value == item:
-      index_list.append([index])
-    elif isinstance(search_list[index], list):
-      for i in index_all(searchlist[index], item):
-        index_list.append([index] + i)
-  return index_list
+    """find all list items"""
+    #searching 'example = [[[1,2,3], 2, [1,3]], [1,2,3]]' for presence of 'example, 2' results in '[[0, 0, 1], [0, 1], [1, 1]]'
+    #searching 'example = [[[1,2,3], 2, [1,3]], [1,2,3]]' for presence of 'example, [1,2,3]' results in '[[0,0], [1]]'
+    index_list = []
+    for index, value in enumerate(search_list):
+        if value == item:
+            index_list.append([index])
+        elif isinstance(search_list[index], list):
+            for i in index_all(search_list[index], item):
+                index_list.append([index] + i)
+    return index_list
 
-#play the waiting game
-def waiting_game():
-  target = random.randint(2,4) #target seconds to wait
-  print(f'\nYour target time is {target} seconds.')
-  
-  input(' ---Press Enter to begin--- ')
-  start = time.perf_counter()
-  
-  input(f'\n...Press Enter again after {target} seconds...')
-  elapsed = time.perf_counter() - start
-  
-  print(f'\nElapsed Time: {elapsed :.3f} seconds')
-  if elapsed == target:
-    print('(Unbelievable! Perfect timing!)')
-  elif elapsed < target:
-    print(f'({target - elapsed :.3f} seconds too fast)')
-  else:
-    print(f'({target - elapsed :.3f} seconds too slow)')
 
+def waiting_game(input):
+    """play the waiting game"""
+    target = random.randint(2,4) #target seconds to wait
+    print(f'\nYour target time is {target} seconds.')
+
+    input(' ---Press Enter to begin--- ')
+    start = time.perf_counter()
+
+    input(f'\n...Press Enter again after {target} seconds...')
+    elapsed = time.perf_counter() - start
+
+    print(f'\nElapsed Time: {elapsed :.3f} seconds')
+    if elapsed == target:
+        print('(Unbelievable! Perfect timing!)')
+    elif elapsed < target:
+        print(f'({target - elapsed :.3f} seconds too fast)')
+    else:
+        print(f'({target - elapsed :.3f} seconds too slow)')
+
+
+def save_dict(dict_to_save, filepath):
+    """save a dictionary"""
+    with open(filepath, 'wb') as file:
+        pickle.dump(dict_to_save, file)
+
+def load_dict(filepath):
+    """load a dictionary"""
+    with open(filepath, "rb") as file:
+        return pickle.load(file)
+
+"""
+test_dict = {1: 'a', 2: 'b', 3: 'c'}
+    save_dict(test_dict, "test.pickle")
+    recovered = load_dict('test.pickle')
+    print(recovered)
+"""
+
+
+def schedule_function():
+    """schedule a function"""
+
+
+def main():
+    """ main function """
+    test_dict = {1: 'a', 2: 'b', 3: 'c'}
+    save_dict(test_dict, "test.pickle")
+    recovered = load_dict('test.pickle')
+    print(recovered)
+
+if __name__ == '__main__':
+    main()
