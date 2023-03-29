b74e20efd9fb5ec5e19b2187606d9f4fec855641
Ben Burbank <91215159+BenagainB@users.noreply.github.com>
2023-03-28 22:21:41 -0700

    Create LevelUpPython.py

---
+++ LevelUpPython.py
@@ -0,0 +1,15 @@
+#Lessons from the Level Up: Python LinkedIn Learning Series
+#https://www.linkedin.com/learning/level-up-python
+
+def get_prime_factors(number):
+  factors = []
+  divisor = 2
+  while divisor <= number:
+    if number % divisor == 0:
+      factors.append(divisor)
+      number = number // divisor
+    else:
+      divisor += 1
+  return factors
+
+
